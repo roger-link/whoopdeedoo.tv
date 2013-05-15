daaf33735c18387ce4adfe60da11b6ca58646112
Ubuntu <ubuntu@ip-10-253-70-238.us-west-2.compute.internal>
2013-05-15 21:32:41 +0000

    adding settings updates

--- wdd/urls.py
+++ wdd/urls.py
@@ -1,9 +1,14 @@
+from django.contrib import admin
 
 from django.conf.urls.defaults import patterns
 from django.views.generic import TemplateView
+
+admin.autodiscover()
  
 urlpatterns = patterns('',
     (r'^test_static/', TemplateView.as_view(template_name="test_static.html")),
+    (r'^admin/', include(admin.site.urls)),
+
 )
 
 
